3685169048d1e2e7c269e462f1cd0a2dccec3f14
Lucas <contato.lucasazevedo1@gmail.com>
2025-06-11 18:50:48 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio003/', // importante para GitHub Pages
   plugins: [react()],
 })
